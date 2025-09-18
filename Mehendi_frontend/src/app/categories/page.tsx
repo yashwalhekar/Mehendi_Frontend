122b3c669427785938f5252089ac4b8de16aeba3
yashwalhekar <yashwalhekar371@gmail.com>
2025-09-18 17:27:55 +0530

    18-9-2025

--- Mehendi_frontend/src/app/categories/page.tsx
+++ Mehendi_frontend/src/app/categories/page.tsx
@@ -3,16 +3,7 @@ import ServicesPage from "./ServicePage";
 export default function CategoriesPage({
   searchParams,
 }: {
-  searchParams?:
-    | { [key: string]: string | string[] | undefined }
-    | Promise<any>;
+  searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  // If Next.js gave us a Promise, resolve it
-  if (searchParams instanceof Promise) {
-    throw new Error(
-      "searchParams is unexpectedly a Promise. Update Next.js types."
-    );
-  }
-
   return <ServicesPage type={searchParams?.type as string | undefined} />;
 }
