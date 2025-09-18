b7ae91ce5618ed0d9e520cbd5f5e9391fbf8c82f
yashwalhekar <yashwalhekar371@gmail.com>
2025-09-18 17:22:07 +0530

    18-9-2025

--- Mehendi_frontend/src/app/categories/page.tsx
+++ Mehendi_frontend/src/app/categories/page.tsx
@@ -1,9 +1,9 @@
 import ServicesPage from "./ServicePage";
 
-export default function CategoriesPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) {
-  return <ServicesPage type={searchParams.type} />;
+interface PageProps {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export default function CategoriesPage({ searchParams }: PageProps) {
+  return <ServicesPage type={searchParams?.type as string | undefined} />;
 }
